9fe42c861a5e958436ddd4875cc8e71fa80c1330
jaoviitor <joaoviiitorvieira@gmail.com>
2023-11-01 21:12:07 -0300

    add swagger

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
 const apiVersion = '/api/v1';
 
 const rotaAluno = require('./routes/aluno');
+
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -25,6 +27,96 @@ app.use((req, res, next) =>{
 })
 
 app.use(`${apiVersion}/aluno`, rotaAluno);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup({
+    "openapi": "3.0.0",
+    "info": {
+        "title": "Desafio API",
+        "description": "API para o desafio do bootcamp Desenvolver WEB/Cloud AWS da Generation Brasil",
+        "version": "1.0.0"
+    },
+    "servers": [
+        {
+        "url": "https://api-desafio-aws.onrender.com",
+        "desciption": "API no ar"
+        }
+    ],
+    "paths": {
+        "/aluno/cadastro": {
+            "post":{
+                "sumary": "Cadastro de aluno",
+                "description": "Essa rota é responsável por cadastrar um aluno",
+                "tags": ["Aluno"],
+                "requestBody":{
+                    "content": {
+                        "aplicattion/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Aluno"
+                                },
+                                "examples": {
+                                    "Aluno": {
+                                        "value": {
+                                            "nome": "João Vitor",
+	                                        "idade": 16,
+	                                        "nota_1": 9.5,
+	                                        "nota_2": 8.5,
+	                                        "professor": "José",
+	                                        "sala": 201
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "responses": {
+                        "500": {
+                            "description": "Internal Server Error"
+                        },
+                        "201": {
+                            "description": "Aluno cadastrado com sucesso!",
+                            "content": {
+                                "application/json": {
+                                    "schema": {
+                                        "type": "object",
+                                        "itens": {
+                                            "$ref": "#/components/schemas/Aluno"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "components": {
+            "schemas": {
+                "Aluno": {
+                    "type": "object",
+                    "properties": {
+                        "nome": {
+                            "type": "string"
+                        },
+                        "idade": {
+                            "type": "number"
+                        },
+                        "nota_1": {
+                            "type": "number"
+                        },
+                        "nota_2": {
+                            "type": "number"
+                        },
+                        "professor": {
+                            "type": "string"
+                        },
+                        "sala": {
+                            "type": "number"
+                        }
+                    }
+                }
+            }
+        }
+    },
+));
 
 //TRATAMENTO PARA QUANDO NÃO FOR ENCONTRADO UMA ROTA
 app.use((req, res, next) =>{
---
+++ swagger.js
@@ -0,0 +1,9 @@
+const options = {
+    "openapi": "3.0.0",
+    "info": {
+        "title": "Desafio API",
+        "description": "API para o desafio do bootcamp Desenvolver WEB/Cloud AWS da Generation Brasil"
+    },
+    "version": "1.0.0"
+};
+module.exports = options;
